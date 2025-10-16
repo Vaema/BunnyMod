ea92d654114b58a073ba7aec62a0aa19301ac4d5
Vaema <vaema.arcadia@gmail.com>
2025-10-16 18:27:35 -0400

    Vortex Bunny.

---
+++ Items/VortexBunnyItem.cs
@@ -0,0 +1,17 @@
+﻿using BunnyMod.NPCs;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BunnyMod.Items;
+
+public class VortexBunnyItem : BaseBunnyItem
+{
+    public override void SafeSetDefaults()
+    {
+        Item.makeNPC = ModContent.NPCType<VortexBunny>();
+        Item.rare = ItemRarityID.Red;
+        Item.value = Item.sellPrice(gold: 1);
+    }
+}
---
+++ NPCs/VortexBunny.cs
@@ -0,0 +1,21 @@
+﻿using BunnyMod.Items;
+
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BunnyMod.NPCs;
+
+public class VortexBunny : BaseBunnyNPC
+{
+    public override int CatchItem => ModContent.ItemType<VortexBunnyItem>();
+
+    public override int DustType => DustID.Vortex;
+
+    public override float SpawnChance(NPCSpawnInfo spawnInfo)
+    {
+        if (!spawnInfo.Player.ZoneTowerVortex)
+            return 0f;
+
+        return 0.2f;
+    }
+}
